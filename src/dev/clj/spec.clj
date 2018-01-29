bc1b22c40058e162dc89e75e617019eda3529239
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-01-29 15:32:32 -0800

    Postwalk through aliases, multis and records just fine

--- src/dev/clj/spec.clj
+++ src/dev/clj/spec.clj
@@ -2,28 +2,35 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.core.match :refer [match]]))
 
-(defn walk-with-spec [spec obj]
-  (println spec obj)
-  
-  (if-let [spec* (s/get-spec spec)]
-    (match [(s/describe* spec*)]
+(defn keys-as-map [keys-form]
+  (let [[_keys & {:keys [req req-un opt opt-un] :as opts}] keys-form]
+    (->> (concat (map (juxt #(keyword (name %)) identity) req-un)
+                 (map (juxt #(keyword (name %)) identity) opt-un)
+                 (map (juxt identity identity) req)
+                 (map (juxt identity identity) opt))
+         (into {}))))
+
+(defn postwalk-with-spec [spec obj f]
+  (if-let [spec* (some-> spec s/get-spec s/describe*)]
+    (match [spec*]
       [([`s/multi-spec mm k] :seq)]
-      (recur ((find-var mm) obj) obj)
-
-      [(s :guard keyword?)]
-      (recur s obj)
-
-      [([`s/keys & args] :seq)]
-      (let [{:keys [req req-un opt opt-un]} (apply hash-map args)]
-        (concat (mapcat (partial apply walk-with-spec)
-                        (keep #(if (contains? obj %)
-                                 [% (get obj %)])
-                              (concat req opt)))
-                (mapcat (partial apply walk-with-spec)
-                        (keep #(if (contains? obj (keyword (name %)))
-                                 [% (get obj (keyword (name %)))])
-                              (concat req-un opt-un)))))
+      (recur ((find-var mm) obj) obj f)
       
-      [s]
-      [[s obj]])
+      [(s :guard keyword?)]
+      (recur s obj f)
+      
+      :else
+      (f spec
+         (match [spec*]
+           [([`s/keys & args] :seq)]
+           (let [spec-map (keys-as-map spec*)]
+             (->> (for [[k v] obj
+                        :let [spec (get spec-map k)]
+                        :when spec
+                        :let [v' (postwalk-with-spec spec v f)]
+                        :when v']
+                    [k v'])
+                  (into {})))
+
+           :else obj)))
     [spec obj]))
